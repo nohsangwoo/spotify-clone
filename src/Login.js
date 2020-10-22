435f763be32bd5d79aaa0e3cd04b5f402b08186c
nohsangwoo <nsgr12@naver.com>
2020-10-22 19:46:23 +0900

    login part two

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Login.css";
 import spotify_Logo from "./img/spotify_logo.jpg";
+import { accessUrl } from "./spotify";
+
 function Login() {
   return (
     <div className="login">
       <img src={spotify_Logo} alt="spotify Logo" />
-      <a href="">LOGIN WITH SPOTIFY</a>
+      <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,28 @@
+export const authEndpoint = process.env.REACT_APP_AUTH_ENDPOINT;
+
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+// spotify에서 인증하고 localhost:3000으로 돌아옴 나중에 호스팅하면 저 주소 바꿔주면됨
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
