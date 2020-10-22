869a8002ae6477fb5b51411673c3e7fc3c84f0bd
nohsangwoo <nsgr12@naver.com>
2020-10-22 23:42:37 +0900

    install spotify web api

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 https://www.youtube.com/watch?v=pnkuI8KXW_8
 https://developer.spotify.com/dashboard/applications/
-16:30
+
+npm i spotify-web-api-js
--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import "./App.css";
-
+import { getTokenFromResponse } from "./spotify";
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token >>>", token);
+  }, [token]);
   return (
     // BEM
-    <div className="App">
-      <Login />
-    </div>
+    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
