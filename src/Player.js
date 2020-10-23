60c46aed2162af61e63e6efb726c3bcda9321130
nohsangwoo <nsgr12@naver.com>
2020-10-23 10:58:54 +0900

    progress to Player part

--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
     <div className="player">
-      <div className="player">
-        {/* Sidebbar */}
-        {/* Body */}
+      <div className="player__body">
+        <Sidebar />
+        <Body />
       </div>
 
-      {/* Footer */}
+      <Footer />
     </div>
   );
 }
