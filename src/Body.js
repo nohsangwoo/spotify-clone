4cb4b8a45fd8e59ae348b7540b3eea2962f3a1b9
nohsangwoo <nsgr12@naver.com>
2020-10-24 21:11:08 +0900

    progress to body part

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,46 @@
+import { PlaylistAdd } from "@material-ui/icons";
 import React from "react";
 import "./Body.css";
+import Header from "./Header";
+import { useStateValue } from "./StateProvider";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
-function Body() {
+function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateValue();
+
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZEVXcJZyENOWUFo7`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
   return (
     <div className="body">
-      <h1>I am the body</h1>
+      <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>Playlist</strong>
+          <h2>Discover Weekly</h2>
+          <p>description</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+import { useStateValue } from "./StateProvider";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { useEffect } from "react";
+
+function Header() {
+  const [{ user }, dispatch] = useStateValue();
+  console.log("user in header", user);
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input type="text" placeholder="Search for Artists, Songs, Podcasts" />
+      </div>
+
+      <div className="header__right">
+        <Avatar alt={user?.display_name} src={user?.images[0]?.url} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
